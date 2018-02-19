c5c22297a3ce1bb9bad47ba09374e9491c087833
Guillaume Leroi <guillaume-externe.leroi@enedis.fr>
2018-02-19 17:58:49 +0100

    mars_lander/ep2: next position prediction ok

--- puzzle/mars_lander/ep2/ep2.go
+++ puzzle/mars_lander/ep2/ep2.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"math"
+	"os"
+)
 
 //import "os"
 
@@ -23,31 +28,152 @@ func min(a, b int) int {
 	return b
 }
 
-func main() {
-	// surfaceN: the number of points used to draw the surface of Mars.
-	var surfaceN int
-	fmt.Scan(&surfaceN)
+func distance(a, b Vec) float64 {
+	return math.Sqrt(float64((b.X-a.X)*(b.X-a.X) + (b.Y-a.Y)*(b.Y-a.Y)))
+}
 
+type Vec struct {
+	X, Y float64
+}
+
+func Vec2f(x, y float64) Vec {
+	return Vec{X: x, Y: y}
+}
+
+func (v Vec) Scale(s float64) Vec {
+	return Vec2f(v.X*s, v.Y*s)
+}
+
+func (v Vec) Round() Vec {
+	return Vec2f(math.Round(v.X), math.Round(v.Y))
+}
+
+func add(a, b Vec) Vec {
+	return Vec2f(a.X+b.X, a.Y+b.Y)
+}
+
+const GravityVal = 3.711
+
+var Gravity = Vec2f(0, -GravityVal)
+
+type surface struct {
+	points []Vec
+}
+
+func readSurface(r io.Reader, debug bool) *surface {
+	var surface surface
+	var surfaceN int
+	fmt.Fscan(r, &surfaceN)
+
+	surface.points = make([]Vec, surfaceN)
 	for i := 0; i < surfaceN; i++ {
-		// landX: X coordinate of a surface point. (0 to 6999)
-		// landY: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-		var landX, landY int
-		fmt.Scan(&landX, &landY)
+		fmt.Fscan(r, &surface.points[i].X, &surface.points[i].Y)
 	}
+	if debug {
+		fmt.Fprintln(os.Stderr, surfaceN)
+		for _, pt := range surface.points {
+			fmt.Fprintln(os.Stderr, pt.X, pt.Y)
+		}
+	}
+	return &surface
+}
+
+func (s *surface) LandingZone() (Vec, Vec) {
+	var a, b Vec
+	//There is a unique area of flat ground on the surface of Mars,
+	// which is at least 1000 meters wide.
+
+	a = s.points[0]
+	for _, pt := range s.points {
+		if pt.Y == a.Y {
+			b = pt
+		} else {
+			// if landing zone is big enough returns it
+			// else restart at pt
+			if distance(a, b) >= 1000 {
+				return a, b
+			}
+			a = pt
+			b = pt
+		}
+	}
+	if distance(a, b) >= 1000 {
+		return a, b
+	}
+	panic("landing zone not found!")
+}
+
+type Force int
+
+type Angle int
+
+type lander struct {
+	Pos      Vec
+	Speed    Vec
+	Fuel     int
+	Rotation Angle // +/- 15
+	Power    Force // +/- 1
+}
+
+func radian(a Angle) float64 {
+	r := float64(a) * 180 / math.Pi
+	return math.Pi/2 + r
+}
+
+func (l lander) Next(timeStep float64, power Force, rotation Angle) lander {
+	n := l
+
+	rad := radian(rotation)
+	accelaration := add(Gravity, Vec2f(math.Cos(rad), math.Sin(rad)).Scale(float64(power)))
+	n.Speed = add(l.Speed, accelaration)
+	n.Pos = add(l.Pos, add(l.Speed, accelaration.Scale(0.5*timeStep)))
+
+	n.Power = power
+	n.Rotation = rotation
+	n.Fuel = l.Fuel - int(power)
+	return n
+}
+
+func (l lander) Round() lander {
+	l.Speed = l.Speed.Round()
+	l.Pos = l.Pos.Round()
+	return l
+}
+
+func readLander(r io.Reader, debug bool) lander {
+	var lander lander
+	fmt.Fscan(r, &lander.Pos.X, &lander.Pos.Y, &lander.Speed.X, &lander.Speed.Y, &lander.Fuel, &lander.Rotation, &lander.Power)
+	if debug {
+		fmt.Fprintln(os.Stderr, lander.Pos.X, lander.Pos.Y, lander.Speed.X, lander.Speed.Y, lander.Fuel, lander.Rotation, lander.Power)
+	}
+	return lander
+}
+
+func main() {
+	/*
+			For a landing to be successful, the ship must:
+
+		    land on flat ground
+		    land in a vertical position (tilt angle = 0°)
+		    vertical speed must be limited ( ≤ 40m/s in absolute value)
+		    horizontal speed must be limited ( ≤ 20m/s in absolute value)
+	*/
+	_ = readSurface(os.Stdin, true)
+
 	for {
 		// hSpeed: the horizontal speed (in m/s), can be negative.
 		// vSpeed: the vertical speed (in m/s), can be negative.
 		// fuel: the quantity of remaining fuel in liters.
 		// rotate: the rotation angle in degrees (-90 to 90).
 		// power: the thrust power (0 to 4).
-		var X, Y, hSpeed, vSpeed, fuel, rotate, power int
-		fmt.Scan(&X, &Y, &hSpeed, &vSpeed, &fuel, &rotate, &power)
+		lander := readLander(os.Stdin, true)
 
 		const minVSpeed = -35
 		const maxPower = 4
 		const minPower = 0
+		power := int(lander.Power)
 		// fmt.Fprintln(os.Stderr, "Debug messages...")
-		if vSpeed < minVSpeed {
+		if lander.Speed.Y < minVSpeed {
 			power = min(power+1, maxPower)
 
 		} else {
---
+++ puzzle/mars_lander/ep2/ep2_test.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+var examples = []struct {
+	input        string
+	landingStart Vec
+	landingEnd   Vec
+}{
+	{
+		input: `7
+		0 100
+		1000 500
+		1500 1500
+		3000 1000
+		4000 150
+		5500 150
+		6999 800
+		2500 2700 0 0 550 0 0`,
+		landingStart: Vec2f(4000, 150),
+		landingEnd:   Vec2f(5500, 150),
+	},
+	{
+		input: `10
+		0 100
+		1000 500
+		1500 100
+		3000 100
+		3500 500
+		3700 200
+		5000 1500
+		5800 300
+		6000 1000
+		6999 2000
+		6500 2800 -100 0 600 90 0`,
+		landingStart: Vec2f(1500, 100),
+		landingEnd:   Vec2f(3000, 100),
+	},
+}
+
+func TestIdentifyLandingZone(t *testing.T) {
+	for testID, test := range examples {
+		r := strings.NewReader(test.input)
+		surface := readSurface(r, false)
+
+		st, end := surface.LandingZone()
+		if test.landingStart != st || test.landingEnd != end {
+			t.Errorf("case %d: expected a landing zone %v -> %v, got %v -> %v (%fm)", testID,
+				test.landingStart, test.landingEnd, st, end, distance(st, end))
+		}
+	}
+}
+
+var landerTests = []struct {
+	steps []string
+}{
+	{
+		steps: []string{
+			"2500 2700 0 0 550 0 0",
+			"2500 2698 0 -4 550 0 0",
+			"2500 2693 0 -7 550 0 0",
+			"2500 2683 0 -11 550 0 0",
+			"2500 2670 0 -15 550 0 0",
+			"2500 2654 0 -19 550 0 0",
+			"2500 2633 0 -22 550 0 0",
+			"2500 2609 0 -26 550 0 0",
+			"2500 2581 0 -30 550 0 0",
+			"2500 2550 0 -33 550 0 0",
+			"2500 2514 0 -37 550 0 0",
+		},
+	},
+	{
+		steps: []string{
+			"2500 2700 0 0 550 0 0",
+			"2500 2698 0 -4 550 0 0",
+			"2500 2693 0 -7 550 0 0",
+			"2500 2683 0 -11 550 0 0",
+			"2500 2670 0 -15 550 0 0",
+			"2500 2654 0 -19 550 0 0",
+			"2500 2633 0 -22 550 0 0",
+			"2500 2609 0 -26 550 0 0",
+			"2500 2581 0 -30 550 0 0",
+			"2500 2550 0 -33 550 0 0",
+			"2500 2514 0 -37 550 0 0",
+			"2500 2476 0 -40 549 0 1",
+			"2500 2435 0 -42 547 0 2",
+			"2500 2393 0 -42 544 0 3",
+			"2500 2351 0 -42 540 0 4",
+			"2500 2310 0 -42 536 0 4",
+			"2500 2268 0 -41 532 0 4",
+			"2500 2227 0 -41 528 0 4",
+			"2500 2186 0 -41 524 0 4",
+			"2500 2145 0 -41 520 0 4",
+			"2500 2105 0 -40 516 0 4",
+			"2500 2065 0 -40 512 0 4",
+			"2500 2025 0 -40 508 0 4",
+			"2500 1985 0 -39 504 0 4",
+			"2500 1946 0 -39 500 0 4",
+			"2500 1907 0 -39 496 0 4",
+			"2500 1869 0 -38 492 0 4",
+			"2500 1830 0 -38 488 0 4",
+			"2500 1792 0 -38 484 0 4",
+			"2500 1755 0 -38 480 0 4",
+			"2500 1717 0 -37 476 0 4",
+			"2500 1680 0 -37 472 0 4",
+			"2500 1643 0 -37 468 0 4",
+			"2500 1606 0 -36 464 0 4",
+			"2500 1570 0 -36 460 0 4",
+			"2500 1534 0 -36 456 0 4",
+			"2500 1498 0 -36 452 0 4",
+			"2500 1463 0 -35 448 0 4",
+			"2500 1427 0 -36 445 0 3",
+			"2500 1391 0 -36 441 0 4",
+			"2500 1356 0 -35 437 0 4",
+			"2500 1320 0 -36 434 0 3",
+			"2500 1284 0 -36 430 0 4",
+			"2500 1248 0 -36 426 0 4",
+			"2500 1213 0 -35 422 0 4",
+			"2500 1177 0 -36 419 0 3",
+		},
+	},
+}
+
+func TestLanderNextPosition(t *testing.T) {
+	for testID, test := range landerTests {
+		current := readLander(strings.NewReader(test.steps[0]), false)
+		for i := 1; i < len(test.steps); i++ {
+			expected := readLander(strings.NewReader(test.steps[i]), false)
+
+			next := current.Next(1, expected.Power, expected.Rotation)
+
+			/*
+				The simulation rounds its result when transmitting it, but seems to use
+				the not rounded result to compute next step.
+				Observed for: Speed
+			*/
+
+			if next.Round() != expected {
+				t.Errorf("case %d: step %d -> %d:\nexpected:\n  %+v\ngot:\n  %+v", testID,
+					i, i+1, expected, next)
+			}
+			current = next
+		}
+	}
+}

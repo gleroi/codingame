2e89ddaebe90df777440af68281ca48a40fe1ac6
Guillaume Leroi <leroi.g@gmail.com>
2018-03-09 00:48:31 +0100

    great_dispatch: far from finished

--- great_dispatch/main.go
+++ great_dispatch/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
-	"math/rand"
 	"os"
-	"time"
 )
 
 //import "os"
@@ -28,6 +26,7 @@ type Box struct {
 
 // Truck carries boxes, with a maximal volume of 100.
 type Truck struct {
+	ID     int
 	Volume float64
 	Weight float64
 }
@@ -51,9 +50,13 @@ func main() {
 	boxes := make([]Box, boxCount)
 	for i := 0; i < boxCount; i++ {
 		fmt.Scan(&boxes[i].W, &boxes[i].V)
+		boxes[i].Truck = -1
 	}
 
 	trucks := make([]Truck, MaxTruck)
+	for t := range trucks {
+		trucks[t].ID = t
+	}
 	simple(boxes, trucks)
 
 	// for each box, print its truck id
@@ -71,56 +74,86 @@ func mean(boxes []Box) (float64, float64) {
 	return MaxTruckVolume, mW / MaxTruck
 }
 
-func delta(trucks []Truck) float64 {
-	min, max := math.MaxFloat64, -1.0
+type Delta struct {
+	Min, Max, Delta float64
+	MinT, MaxT      int
+}
 
+func delta(trucks []Truck) Delta {
+	d := Delta{
+		Min:   math.MaxFloat64,
+		Max:   -math.MaxFloat64,
+		Delta: math.MaxFloat64,
+		MinT:  -1,
+		MaxT:  -1,
+	}
+	if trucks == nil {
+		return d
+	}
+	return d.Update(trucks...)
+}
+
+func (d Delta) Update(trucks ...Truck) Delta {
 	for _, t := range trucks {
-		if t.Weight < min {
-			min = t.Weight
+		if d.MinT == t.ID {
+			d.Min = t.Weight
 		}
-		if t.Weight > max {
-			max = t.Weight
+		if d.MaxT == t.ID {
+			d.Max = t.Weight
+		}
+		if t.Weight < d.Min {
+			d.Min = t.Weight
+			d.MinT = t.ID
+		}
+		if t.Weight > d.Max {
+			d.Max = t.Weight
+			d.MaxT = t.ID
 		}
 	}
-
-	return max - min
+	d.Delta = d.Max - d.Min
+	return d
 }
 
 func simple(boxes []Box, trucks []Truck) {
-	for b, box := range boxes {
-		done := false
-		for t, truck := range trucks {
-			trucks[t], done = truck.Add(box)
-			if done {
-				boxes[b].Truck = t
-				break
+	unplacedBox := true
+
+	iter := 0
+	for unplacedBox {
+		unplacedBox = false
+		unplacedCount := 0
+
+		for b := 0; b < len(boxes); b++ {
+			box := boxes[b]
+			if box.Truck != -1 {
+				continue
+			}
+			minT := -1
+			deltaMin := delta(trucks)
+
+			for t, truck := range trucks {
+				if truck.Weight == 0 {
+					minT = t
+					break
+				}
+				nt, ok := truck.Add(box)
+				if ok {
+					nd := deltaMin.Update(nt)
+					if nd.Delta < deltaMin.Delta {
+						deltaMin = nd
+						minT = t
+					}
+				}
+			}
+			if minT != -1 {
+				trucks[minT], _ = trucks[minT].Add(box)
+				boxes[b].Truck = minT
+			} else {
+				unplacedBox = true
+				unplacedCount++
 			}
 		}
-		if !done {
-			debug("box %d: %v with not trucks\n", b, box)
-		}
-	}
-}
-
-func annealing(boxes []Box, trucks []Truck) {
-	delta := delta(trucks)
-	randMax := len(boxes)
-	rng := rand.New(rand.NewSource(42))
-
-	start := time.Now()
-	for delta > 0 && time.Now().Sub(start).Seconds() < 49.988 {
-		b1 := rng.Intn(randMax)
-		b2 := rng.Intn(randMax)
-		if b1 == b2 {
-			continue
-		}
-		if boxes[b1].Truck == boxes[b2].Truck {
-			continue
-		}
-		// validate swap
-
-		// validate better delta
-
+		iter++
+		debug("iter %d: %d remaining box\n", iter, unplacedCount)
 	}
 }
 

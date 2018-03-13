b0af4d197a4b9d88cf742177f417367db825bef4
Guillaume Leroi <leroi.g@gmail.com>
2018-03-14 00:58:50 +0100

    great_dispatch: try to optimize by swapping

--- great_dispatch/main.go
+++ great_dispatch/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 )
 
 //import "os"
@@ -40,6 +41,12 @@ func (t Truck) Add(b Box) (Truck, bool) {
 	return t, false
 }
 
+func (t Truck) Remove(b Box) Truck {
+	t.Volume -= b.V
+	t.Weight -= b.W
+	return t
+}
+
 const MaxTruckVolume = 100
 const MaxTruck = 100
 
@@ -50,14 +57,15 @@ func main() {
 	boxes := make([]Box, boxCount)
 	for i := 0; i < boxCount; i++ {
 		fmt.Scan(&boxes[i].W, &boxes[i].V)
-		boxes[i].Truck = -1
+		boxes[i].Truck = 0
 	}
 
 	trucks := make([]Truck, MaxTruck)
 	for t := range trucks {
 		trucks[t].ID = t
 	}
-	simple(boxes, trucks)
+	firstfit(boxes, trucks)
+	optimize(boxes, trucks)
 
 	// for each box, print its truck id
 	for _, box := range boxes {
@@ -79,22 +87,26 @@ type Delta struct {
 	MinT, MaxT      int
 }
 
-func delta(trucks []Truck) Delta {
-	d := Delta{
+func NewDelta() Delta {
+	return Delta{
 		Min:   math.MaxFloat64,
 		Max:   -math.MaxFloat64,
 		Delta: math.MaxFloat64,
 		MinT:  -1,
 		MaxT:  -1,
 	}
-	if trucks == nil {
-		return d
-	}
+}
+
+func delta(trucks []Truck) Delta {
+	d := NewDelta()
 	return d.Update(trucks...)
 }
 
 func (d Delta) Update(trucks ...Truck) Delta {
 	for _, t := range trucks {
+		if t.Weight == 0 {
+			continue
+		}
 		if d.MinT == t.ID {
 			d.Min = t.Weight
 		}
@@ -114,46 +126,131 @@ func (d Delta) Update(trucks ...Truck) Delta {
 	return d
 }
 
-func simple(boxes []Box, trucks []Truck) {
-	unplacedBox := true
+func firstfit(boxes []Box, trucks []Truck) {
+	bIds := make([]int, len(boxes))
+	for id := range boxes {
+		bIds[id] = id
+	}
+	sort.Slice(bIds, func(i, j int) bool {
+		return boxes[i].W >= boxes[j].W
+	})
 
-	iter := 0
-	for unplacedBox {
-		unplacedBox = false
-		unplacedCount := 0
+	for _, b := range bIds {
+		box := boxes[b]
 
-		for b := 0; b < len(boxes); b++ {
-			box := boxes[b]
-			if box.Truck != -1 {
-				continue
-			}
-			minT := -1
-			deltaMin := delta(trucks)
-
-			for t, truck := range trucks {
-				if truck.Weight == 0 {
-					minT = t
-					break
+		delta := NewDelta()
+		bestT := -1
+		for t, truck := range trucks {
+			nt, ok := truck.Add(box)
+			if ok {
+				nd := delta.Update(nt)
+				if nd.Delta < delta.Delta {
+					bestT = t
+					delta = nd
 				}
-				nt, ok := truck.Add(box)
-				if ok {
-					nd := deltaMin.Update(nt)
-					if nd.Delta < deltaMin.Delta {
-						deltaMin = nd
-						minT = t
-					}
-				}
-			}
-			if minT != -1 {
-				trucks[minT], _ = trucks[minT].Add(box)
-				boxes[b].Truck = minT
-			} else {
-				unplacedBox = true
-				unplacedCount++
 			}
 		}
-		iter++
-		debug("iter %d: %d remaining box\n", iter, unplacedCount)
+		if bestT == -1 {
+			debug("box %d not placed\n", b)
+		} else {
+			trucks[bestT], _ = trucks[bestT].Add(box)
+			boxes[b].Truck = bestT
+		}
+	}
+}
+
+func findBoxes(boxes []Box, t int) []int {
+	ids := make([]int, 0, len(boxes))
+	for b, box := range boxes {
+		if box.Truck == t {
+			ids = append(ids, b)
+		}
+	}
+	return ids
+}
+
+func optimize(boxes []Box, trucks []Truck) {
+	for iter := 0; iter < 5000; iter++ {
+		delta := delta(trucks)
+		debug("delta: %f, min: %d, max: %d\n", delta.Delta, delta.MinT, delta.MaxT)
+		debug("delta: min: %f, max: %f\n", delta.Min, delta.Max)
+		boxesMin := findBoxes(boxes, delta.MinT)
+		boxesMax := findBoxes(boxes, delta.MaxT)
+		b1, b2 := -1, -1
+		d := math.MaxFloat64
+		for _, bMin := range boxesMin {
+			boxMin := boxes[bMin]
+			for _, bMax := range boxesMax {
+				boxMax := boxes[bMax]
+				deltaBoxes := math.Abs(boxMax.W - boxMin.W)
+				dNext := math.Abs(delta.Delta - deltaBoxes)
+				if dNext < d && canSwap(boxes, trucks, bMin, bMax) {
+					b1 = bMin
+					b2 = bMax
+					d = dNext
+				}
+			}
+		}
+		if b1 != -1 && b2 != -1 {
+			swap(boxes, trucks, b1, b2)
+		} else {
+			debug("remaining iter %d\n", iter)
+			return
+		}
+	}
+}
+
+func canSwap(boxes []Box, trucks []Truck, b1Id int, b2Id int) bool {
+	b1, b2 := boxes[b1Id], boxes[b2Id]
+	t1, t2 := trucks[b1.Truck], trucks[b2.Truck]
+
+	t1 = t1.Remove(b1)
+	t2 = t2.Remove(b2)
+
+	var ok bool
+	t1, ok = t1.Add(b2)
+	if !ok {
+		return false
+	}
+	t2, ok = t2.Add(b1)
+	return ok
+}
+
+func swap(boxes []Box, trucks []Truck, b1Id int, b2Id int) {
+	b1, b2 := boxes[b1Id], boxes[b2Id]
+	t1, t2 := trucks[b1.Truck], trucks[b2.Truck]
+
+	t1 = t1.Remove(b1)
+	t2 = t2.Remove(b2)
+
+	var ok bool
+	t1, ok = t1.Add(b2)
+	if !ok {
+		panic(fmt.Errorf("cannot swap %d from %d to %d\n", b2Id, t2.ID, t1.ID))
+	}
+	t2, ok = t2.Add(b1)
+	if !ok {
+		panic(fmt.Errorf("cannot swap %d from %d to %d\n", b1Id, t1.ID, t2.ID))
+	}
+	debug("swap\n  %+v\nand\n  %+v\n", b1, b2)
+	trucks[t1.ID] = t1
+	boxes[b2Id].Truck = t1.ID
+	trucks[t2.ID] = t2
+	boxes[b1Id].Truck = t2.ID
+}
+
+func fillTrucks(boxes []Box, trucks []Truck) {
+	for b := 0; b < len(boxes); b++ {
+		box := boxes[b]
+
+		for t, truck := range trucks {
+			nt, ok := truck.Add(box)
+			if ok {
+				trucks[t] = nt
+				boxes[b].Truck = nt.ID
+				break
+			}
+		}
 	}
 }
 
---
+++ great_dispatch/problem.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+
+Problem
+
+Given N boxes Bj of weight and volume Wj, Vj
+and M trucks Ti of capacity Ci (max = 100)
+
+A_ij = 1 if Bj is in Ti else 0
+
+Find a solution such as:
+ - Each truck has a volume max of 100: for each truck Ti: Sum Aij * Vj <= 100
+ - Each box is one truck only: for each box Bj: Sum Aij = 1
+ - The difference between the heavier and lighter truck is minimal:
+   Max(Sum Wj) - Min(Sum Wj) is minimal
+
+Given i : Sum Aij * Vj + Si = 100
+Given j : Sum Aij = 1
+Argmin_i Sum_j(AijWj) - Argmax_i(AijWj)
+
+
+*/
